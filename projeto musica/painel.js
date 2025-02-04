9c716361d63ac642a215a1788fd2f96315d5236c
hebert <heberte.2004@gmail.com>
2025-02-04 11:27:23 -0300

    atualizacao

--- projeto musica/painel.js
+++ projeto musica/painel.js
@@ -1,27 +1,55 @@
 
-    let currentIndex = 0;
-    const painéis = document.querySelectorAll('.conteudo-principal .paineis .imagem-painel');
-    const btnVoltar = document.querySelector('.btn-voltar');
-    const btnAvancar = document.querySelector('.btn-avancar');
+    // let currentIndex = 0;
+    // const painéis = document.querySelectorAll('.conteudo-principal .titulo');
+    // const btnVoltar = document.querySelector('.btn-voltar');
+    // const btnAvancar = document.querySelector('.btn-avancar');
 
-    function atualizarCarrossel() {
-        painéis.forEach((painel, index) => {
-            painel.classList.remove('mostrar');
-            if (index === currentIndex) {
-                painel.classList.add('mostrar');
-            }
-        });
+    // function atualizarCarrossel() {
+    //     painéis.forEach((titulo, index) => {
+    //         painel.classList.remove('mostrar');
+    //         if (index === currentIndex) {
+    //             painel.classList.add('mostrar');
+    //         }
+    //     });
+    // }
+
+    // btnVoltar.addEventListener('click', () => {
+    //     currentIndex = (currentIndex === 0) ? titulo.length - 1 : currentIndex - 1;
+    //     atualizarCarrossel();
+    // });
+
+    // btnAvancar.addEventListener('click', () => {
+    //     currentIndex = (currentIndex === titulo.length - 1) ? 0 : currentIndex + 1;
+    //     atualizarCarrossel();
+    // });
+
+    // // Inicializa o carrossel exibindo a primeira imagem
+    // atualizarCarrossel();
+
+
+    const dropdown = document.querySelector('.dropdown');
+const dropdownContent = document.querySelector('.dropdown-content');
+const box = document.querySelector(".slider")
+const image = document.querySelector(".slider img")
+
+let conter = 0;
+
+function slider() {
+    conter++;
+
+    if(conter >= 3) {
+       conter = 0; 
     }
 
-    btnVoltar.addEventListener('click', () => {
-        currentIndex = (currentIndex === 0) ? painéis.length - 1 : currentIndex - 1;
-        atualizarCarrossel();
-    });
+    box.style.transform = `translateX(${-conter*800}px)`;
+}
 
-    btnAvancar.addEventListener('click', () => {
-        currentIndex = (currentIndex === painéis.length - 1) ? 0 : currentIndex + 1;
-        atualizarCarrossel();
-    });
+setInterval(slider, 4000);
 
-    // Inicializa o carrossel exibindo a primeira imagem
-    atualizarCarrossel();
+dropdown.addEventListener('mouseover', () => {
+    dropdownContent.classList.add('show');
+});
+
+dropdown.addEventListener('mouseout', () => {
+    dropdownContent.classList.remove('show');
+});
